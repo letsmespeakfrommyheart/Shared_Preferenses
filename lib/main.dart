e6572e0d3dfcbe98ff88f52ba9d0145120c6b01d
letsmespeakfrommyheart <maria@MacBook-Air-Vladimir.local>
2022-05-22 01:32:20 +0300

    upgrade app

--- lib/main.dart
+++ lib/main.dart
@@ -8,158 +8,213 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginDemo(),
+      home: LoginPage(),
     );
   }
 }
 
-class LoginDemo extends StatefulWidget {
-  const LoginDemo({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
   @override
   _LoginDemoState createState() => _LoginDemoState();
 }
 
-class _LoginDemoState extends State<LoginDemo> {
-  dynamic getName = SharedPreferences.getInstance();
-  dynamic getPass = SharedPreferences.getInstance();
+class _LoginDemoState extends State<LoginPage> {
+  dynamic getStorage = SharedPreferences.getInstance();
   TextEditingController logUserController = TextEditingController();
   TextEditingController logPassController = TextEditingController();
-  String inputUserName = "";
-  String inputPass = "";
-  var name = "";
-  var pass = "";
+  String? _name;
+  String? _pass;
 
-  // void getUsername() async {
-  //   final getName = await SharedPreferences.getInstance();
-  //   setState(() {
-  //     var userName = getName.getString('name');
-  //     print(userName);
-  //   });
-  // }
-
-  Future getDataName() async {
-    getName = await SharedPreferences.getInstance();
-    String name = await getName.getString('username');
-    return name;
+  Future<String?> getData() async {
+    getStorage = await SharedPreferences.getInstance();
+    dynamic name = await getStorage.get('username');
+    dynamic pass = await getStorage.get('password');
+    setState(() {
+      _name = name;
+      _pass = pass;
+    });
   }
 
-  Future getDataPass() async {
-    getPass = await SharedPreferences.getInstance();
-    String pass = await getPass.getString('password');
-    return pass;
+  String? answer() {
+    if ((_name == logUserController.text) &&
+        (_pass == logPassController.text)) {
+      return ('Hello, $_name, nice to see you again!');
+    } else if ((_name != logUserController.text) |
+        (_pass != logPassController.text)) {
+      return ('The username or password is incorrect, please try again');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text("Login Page"),
-      ),
-      body: Center(
-        child: Container(
-          height: 500,
-          width: 350,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: TextField(
-                  controller: logUserController,
-                  onSubmitted: (dynamic input) {
-                    inputUserName = input;
-                    setState(() {
-                      print(inputUserName);
-                    });
-                  },
-                  // onSubmitted: (String inputUsername) {
-                  //   input = inputUsername;
-                  //   setState(() {
-                  //     setUsername();
-                  //     print(input);
-                  //   });
-                  // },
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Username',
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 15.0, right: 15.0, top: 15, bottom: 0),
-                //padding: EdgeInsets.symmetric(horizontal: 15),
-                child: TextField(
-                  controller: logPassController,
-                  onSubmitted: (dynamic input) {
-                    inputPass = input;
-                    setState(() {
-                      print(inputPass);
-                    });
-                  },
-                  obscureText: true,
-                  obscuringCharacter: '*',
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Container(
-                height: 50,
-                width: 250,
-                decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(20)),
-                child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      print(name);
-                      print(pass);
-                      if ((name == inputUserName) & (pass == inputPass)) {
-                        print('Ura!!');
-                      } else if (name != inputUserName) {
-                        print('Repeat UserName');
-                      } else if (pass != inputPass) {
-                        print('Repeat Password');
-                      }
-                    });
-                  },
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(color: Colors.white, fontSize: 25),
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Don't have an account?"),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (_) => SignUpPage()));
-                    },
-                    child: const Text(
-                      'Sign up',
-                      style: TextStyle(color: Colors.blue, fontSize: 15),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text(
+            '',
+            style: TextStyle(color: Colors.white),
           ),
         ),
-      ),
-    );
+        body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+            ),
+            child: Center(
+                child: Container(
+              height: 500,
+              width: 350,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 20,
+                      // spreadRadius: 3,
+                      color: Colors.black.withOpacity(0.5))
+                ],
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(60),
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Log in form',
+                        style: TextStyle(
+                            fontFamily: 'GrapeNuts',
+                            fontSize: 45,
+                            fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 30),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        autocorrect: false,
+                        style: const TextStyle(
+                          fontSize: 17,
+                        ),
+                        cursorHeight: 20,
+                        cursorColor: Colors.black,
+                        controller: logUserController,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(
+                            Icons.sentiment_satisfied_outlined,
+                            color: Colors.black,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                              color: Colors.black,
+                              width: 1.0,
+                            ),
+                          ),
+                          hintText: 'Username',
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 15.0, right: 15.0, top: 15, bottom: 0),
+                      child: TextField(
+                        obscureText: true,
+                        obscuringCharacter: '*',
+                        style: const TextStyle(
+                          fontSize: 17,
+                        ),
+                        cursorHeight: 20,
+                        cursorColor: Colors.black,
+                        controller: logPassController,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(
+                            Icons.vpn_key_outlined,
+                            color: Colors.black,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                              color: Colors.black,
+                              width: 1.0,
+                            ),
+                          ),
+                          hintText: 'Password',
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.black),
+                        fixedSize:
+                            MaterialStateProperty.all(const Size(180, 50)),
+                        shadowColor: MaterialStateProperty.all(Colors.grey),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          getData();
+                        });
+                      },
+                      child: const Text(
+                        'Log in',
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    Column(children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Don't have an account yet?"),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) {
+                                  return const SignUpPage();
+                                }),
+                              );
+                            },
+                            child: const Text(
+                              'Sign up',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w800),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        height: 90,
+                        width: 300,
+                        child: (_name != null) & (_pass != null)
+                            ? Text('${answer()}',
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontFamily: 'GrapeNuts',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 27.0,
+                                  color: Colors.red,
+                                ))
+                            : const Text(''),
+                      ),
+                    ]),
+                  ]),
+            ))));
   }
 }
